5c5da9c8aaf941cbb213dda02f3f6c5dc474fb72
nohturfft <nohturfft@gmail.com>
2021-10-28 22:03:21 +0100

    Working on gap insertion

--- ui.R
+++ ui.R
@@ -14,7 +14,18 @@ shinyUI(
                fileInput("files", "Choose PNG Files", accept = ".png", multiple=TRUE),
                numericInput('size', 'Pixel size', 500, min = 10, max = 1000),
                numericInput('coord.x', 'X Coordinate (top left)', 1, min = 1),
-               numericInput('coord.y', 'Y coordinate (top left)', 1, min = 1)
+               numericInput('coord.y', 'Y coordinate (top left)', 1, min = 1),
+               checkboxInput("check_gap", "Insert a gap", TRUE),
+               conditionalPanel(
+                 condition = "input.check_gap == 1",
+                 tags$head(
+                   tags$style(type="text/css",
+                              "#inline label{ display: table-cell; text-align: center; vertical-align: middle; } 
+                #inline .form-group { display: table-row;}")
+                 ),
+                tags$div(id = "inline", class="x", numericInput('gap', 'Gap size:', 15, min = 0, max = 100))
+               )
+               
         ), # end column
         column(width=8,
                plotOutput("plot1"),
