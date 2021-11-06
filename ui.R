513a664e0a435cd3bacecfb93ea64f913ed26752
nohturfft <nohturfft@gmail.com>
2021-11-06 17:34:25 +0000

    Improving ui

--- ui.R
+++ ui.R
@@ -21,29 +21,31 @@ shinyUI(
       title = "Main",
       fluidPage(
         useShinyjs(),
-        column(width=4,
+        column(width=3,
                align="left",
                fileInput("files", "Choose PNG Files", accept = ".png", multiple=TRUE),
                rHandsontableOutput("hot_files"),
                tags$p(""),
-               numericInput('size', 'Pixel size', 500, min = 10, max = 1000),
-               numericInput('coord.x', 'X Coordinate (top left)', 1, min = 1),
-               numericInput('coord.y', 'Y coordinate (top left)', 1, min = 1),
-               hidden(
-                 plotOutput("plot_overview", width="400px", height="260px", click = "img_click")
+               wellPanel(id="selection_well", 
+                         tags$div(id="numericinput_selection",
+                                  numericInput('size', 'Pixel size', 500, min = 10, max = 1000),
+                                  numericInput('coord.x', 'X Coordinate (top left)', 1, min = 1),
+                                  numericInput('coord.y', 'Y coordinate (top left)', 1, min = 1),
+                         ),
+                         hidden(
+                           plotOutput("plot_overview", width="400px", height="260px", click = "img_click")
+                         )
                ),
-               verbatimTextOutput("click_info"),
-               checkboxInput("check_gap", "Insert a gap", TRUE),
-               conditionalPanel(
-                 condition = "input.check_gap == 1",
-                tags$div(id = "inline", class="x", numericInput('gap', 'Gap size:', 15, min = 0, max = 100)),
-               ), # end conditionalPanel
+               wellPanel(id="composite_well",
+                         checkboxInput("check_gap", "Insert a gap", TRUE),
+                         conditionalPanel(
+                           condition = "input.check_gap == 1",
+                           tags$div(id = "inline", class="x", numericInput('gap', 'Gap size:', 15, min = 0, max = 100)),
+                         ) # end conditionalPanel
+                         ),
                rHandsontableOutput("hot_colors"),
                tags$p(""),
-               # hidden(tags$div(id = "div_download_composite",
-               #                 downloadButton("download", "Download composite")))
                hidden(downloadButton("download", "Download composite"))
-               # downloadButton("download", "Download composite")
         ), # end column
         column(width=8,
                # displayOutput("plot3"),
